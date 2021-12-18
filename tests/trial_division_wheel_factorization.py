58c05a96dd778f11107fb9c5f83ee973afd6e46e
adeetya_tantia <atonlineaccounts+gitlab@pm.me>
2021-12-18 03:25:43 +0000

    changes to trial_division

--- tests/trial_division_wheel_factorization.py
+++ tests/trial_division_wheel_factorization.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def trial_division_wheel_factorization(n):
     is_prime = True
-    if n<=2:
+    if n<=3 or n==5:
 #first checking if n<=2 as then it is obviously prime
         is_prime = True
     elif n%2==0 or n%3==0 or n%5==0:
@@ -12,7 +12,7 @@ def trial_division_wheel_factorization(n):
     else:
 #using Wheel Factorization such that now, every integer can be represented as 30k+i but for i divisible by 2,3,5 the number will not be prime. This leaves the values of i that may be prime as = 1, 7, 11, 13, 17, 19, 23, 29. We use this fact to reduce the number of checks to make.
         max_check = math.ceil(math.sqrt(n))
-        checks = np.array(range(3,max_check,2))
+        checks = np.array(range(3,max_check+1,2))
         checks = [s for s in checks if s%30==1 or s%30==7 or s%30==11 or s%30==13 or s%30==17 or s%30==19 or s%30==23 or s%30==29]
         checker = [n%i for i in checks]
         if 0 in checker:
